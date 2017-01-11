2a8db125be40d19dec2e8a3248bc5a8817ad8b16
Guillermo Hernández <guillehg@gmail.com>
2017-01-11 11:58:31 +0100

    Mock dependencies on RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,22 @@ import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../'))
 
+# Mock the needed packages on RTD
+# http://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+# Make sure to set the interpreter to Cpython 3.X
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    from unittest.mock import MagicMock
+
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return Mock()
+
+    MOCK_MODULES = ['numpy', 'h5py', 'matplotlib', 'matplotlib.pyplot', 'matplot.animation']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
