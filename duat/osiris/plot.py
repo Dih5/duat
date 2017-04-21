fe31e904262ae001a1fad272ea1121edee507bb6
Guillermo Hernández <guillehg@gmail.com>
2017-04-21 17:11:48 +0200

    Unify datasets-as-an-axis and axes treatment

--- duat/osiris/plot.py
+++ duat/osiris/plot.py
@@ -43,6 +43,7 @@ class Diagnostic:
         time_list (`list` of `str`): List of times in each snapshot.
         keys (`list` of `str`): Names of the datasets in the Diagnostic, given in human order.
         axes (`list` of `dict`): Info of each axis in the Diagnostic.
+        datasets_as_axis(`dict`): Info of datasets if treated as axes. WARNING: Only used with energy bins.
         shape (`tuple`): A tuple with:
 
             * `list`: The number of grid dimensions.
@@ -75,6 +76,29 @@ class Diagnostic:
         with h5py.File(self.file_list[0], "r") as f:
             self.keys = self._get_keys(f)
             self.axes = self._get_axes(f, self.keys[0])
+            if len(self.keys) > 1:
+                # Construct an axes-like object representing the dataset
+                self.datasets_as_axis = {}
+                # Guess name
+                name = f[self.keys[0]].attrs["TAG"][0].decode('UTF-8')  # E.g., "Energy <=    0.00000000"
+                name = re.match("(.*?) <", name).group(1)  # TODO: Catch errors, check syntax
+                self.datasets_as_axis["NAME"] = name
+                self.datasets_as_axis["LONG_NAME"] = name
+                # Guess values
+                dataset_axes = []
+                number_pattern = r'[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?'  # A pattern matching floats
+                for d in self.keys:
+                    tag = f[d].attrs["TAG"][0].decode('UTF-8')
+                    # tag is, e.g., '   0.00000000      < Energy <=    5.00000007E-02'
+                    values = list(map(float, re.findall(number_pattern, tag)))
+                    dataset_axes.append(sum(values) / len(values))  # So simple inequalities are represented somehow
+                    # TODO: First and last datasets are not bounded intervals. Their only finite point is used as the x coordinate, which must be understood with caution
+                self.datasets_as_axis["LIST"] = dataset_axes
+                self.datasets_as_axis["MIN"] = dataset_axes[0]
+                self.datasets_as_axis["MAX"] = dataset_axes[-1]
+                self.datasets_as_axis["UNITS"] = "(?)"
+            else:
+                self.datasets_as_axis = None
 
         self.shape = ([len(x["LIST"]) for x in self.axes], len(self.keys), len(self.time_list))
 
@@ -155,7 +179,7 @@ class Diagnostic:
 
         """
         multiple_datasets = False  # If a dataset list is going to be returned
-        if dataset_selector:
+        if dataset_selector is not None:
             if self.shape[1] == 1:
                 print("Single dataset found. Ignoring the provided dataset_selector.")
 
@@ -182,7 +206,7 @@ class Diagnostic:
                 def f_dataset_selector(f):
                     return f[self.keys[0]][:]
 
-        if axes_selector:
+        if axes_selector is not None:
             def f_axes_selector(x):
                 offset = 1 if multiple_datasets else 0  # If multiple dataset, do not count its axis for reduction
                 for i, sel in enumerate(axes_selector):
@@ -211,6 +235,25 @@ class Diagnostic:
 
         return gen()
 
+    def get_axes(self, dataset_selector=None, axes_selector=None):
+        axes = []
+        if dataset_selector is not None:
+            if self.shape[1] == 1:
+                print("Single dataset found. Ignoring the provided dataset_selector.")
+                # Anyhow, datasets are reduced, so skip
+
+        elif self.shape[1] > 1:
+            axes.append(self.datasets_as_axis)
+
+        if axes_selector is not None:
+            for i, sel in enumerate(axes_selector):
+                if sel is None:
+                    axes.append(self.axes[i])
+        else:
+            for a in self.axes:
+                axes.append(a)
+        return axes
+
     def time_1d_animation(self, output_path=None, dataset_selector=None, axes_selector=None, time_selector=None,
                           dpi=200, fps=1, scale_mode="expand",
                           latex_label=True):
@@ -234,14 +277,20 @@ class Diagnostic:
         Returns:
 
         """
+        axes = self.get_axes(dataset_selector=dataset_selector, axes_selector=axes_selector)
+        if len(axes) != 1:
+            raise ValueError("Expected 1 axis plot, but %d were provided" % len(axes))
+        axis = axes[0]
+
         gen = self.get_generator(dataset_selector=dataset_selector, axes_selector=axes_selector,
                                  time_selector=time_selector)
 
         # Set plot labels
         fig, ax = plt.subplots()
         fig.set_tight_layout(True)
-        x_name = self.axes[0]["LONG_NAME"]
-        x_units = self.axes[0]["UNITS"]
+
+        x_name = axis["LONG_NAME"]
+        x_units = axis["UNITS"]
         with h5py.File(self.file_list[0], "r") as f:
             y_name = f[self.keys[0]].attrs["LONG_NAME"][0].decode('UTF-8')
             y_units = f[self.keys[0]].attrs["UNITS"][0].decode('UTF-8')
@@ -259,15 +308,21 @@ class Diagnostic:
         ax.set_ylabel("%s (%s)" % (y_name, y_units))
 
         # Plot the points
-        x_min, x_max = self.axes[0]["MIN"], self.axes[0]["MAX"]
-        plot_data, = ax.plot(self.axes[0]["LIST"], next(gen))
+        x_min, x_max = axis["MIN"], axis["MAX"]
+        plot_data, = ax.plot(axis["LIST"], next(gen))
         ax.set_xlim(x_min, x_max)
+
+        time_list = self.time_list[time_selector] if time_selector else self.time_list
 
         # Prepare a function for the updates
         def update(i):
             """Update the plot, returning the artists which must be redrawn"""
-            new_dataset = next(gen)
-            label = 't = {0}'.format(self.time_list[i])
+            try:
+                new_dataset = next(gen)
+            except StopIteration:
+                print("Warning: Tried to add a frame but all data was used")
+                return
+            label = 't = {0}'.format(time_list[i])
             plot_data.set_ydata(new_dataset[:])
             ax.set_title(label)
             if not scale_mode:
@@ -282,135 +337,22 @@ class Diagnostic:
 
         if not output_path:  # "" or None
             # FIXME: Plot in matplotlib window seems not to be working now.
-            FuncAnimation(fig, update, frames=np.arange(0, len(self.time_list)), interval=1)
+            FuncAnimation(fig, update, frames=np.arange(1, len(time_list) - 1), interval=1)
             plt.show()
         elif output_path.split(".")[-1].lower() == "gif":
-            anim = FuncAnimation(fig, update, frames=np.arange(0, len(self.time_list)), interval=200)
+            anim = FuncAnimation(fig, update, frames=np.arange(1, len(time_list) - 1), interval=200)
             anim.save(output_path, dpi=dpi, writer='imagemagick')
         else:
             metadata = dict(title=os.path.split(self.data_path)[-1], artist='duat', comment=self.data_path)
             writer = FFMpegWriter(fps=fps, metadata=metadata)
             with writer.saving(fig, output_path, dpi):
                 # Iterate over frames
-                for i in np.arange(1, len(self.time_list)):
+                for i in np.arange(1, len(time_list)):
                     update(i)
                     writer.grab_frame()
                 # Keep showing the last frame for the fixed time
                 writer.grab_frame()
         plt.close()
-
-
-# TODO: Recode. DEPRECATED
-def time_dataset_animation(data_path, output_path=None, position=None, dpi=200, fps=1, scale_mode="expand",
-                           latex_label=True):
-    """
-    Generate a dataset-distributed magnitude animated in time.
-
-    Args:
-        data_path(str): The folder containing the files with the slices in time.
-        output_path (str): The place where the plot is saved. If "" or None, the plot is shown in matplotlib.
-        position (int): Number of the position in the axis to plot.
-        dpi (int): The resolution of the file in dots per inch.
-        fps (float): The frames per seconds.
-        scale_mode (str): How the scale is changed thorough time. Available methods are:
-
-            * "expand": The y limits increase when needed, but they don't decrease.
-            * "adjust_always": Always change the y limits to those of the data.
-
-        latex_label (bool): Whether for use LaTeX code for the plot.
-
-    Returns:
-
-    """
-    file_list = glob(os.path.join(data_path, "*.h5"))
-    file_list.sort(key=human_order_key)
-    time_list = list(map(lambda x: float((os.path.split(x)[1]).split(".h5")[0].split("-")[-1]), file_list))
-
-    # Plot the first frame
-    f = h5py.File(file_list[0], "r")
-    keys = list(f.keys())
-
-    # Choose the dataset
-    if "AXIS" not in keys:
-        raise ValueError("AXIS group not found in file %s." % file_list[0])
-    keys.remove("AXIS")
-    keys.sort(key=human_order_key)
-
-    # Set plot labels
-    fig, ax = plt.subplots()
-    fig.set_tight_layout(True)
-    x_name = f[keys[0]].attrs["TAG"][0].decode('UTF-8')  # "Energy <=    0.00000000"
-    x_name = re.match("(.*?) <", x_name).group(1)  # TODO: Catch errors, check syntax
-    y_name = f[keys[0]].attrs["LONG_NAME"][0].decode('UTF-8')
-    y_units = f[keys[0]].attrs["UNITS"][0].decode('UTF-8')
-    if latex_label:
-        y_units = "$" + y_units + "$"
-        # Names might be text or LaTeX. Try to guess
-        if _is_latex(x_name):
-            x_name = "$" + x_name + "$"
-        if _is_latex(y_name):
-            y_name = "$" + y_name + "$"
-    ax.set_xlabel("%s" % x_name)
-    ax.set_ylabel("%s (%s)" % (y_name, y_units))
-
-    # Plot the points
-    x = []
-    y = []
-    if position is None:
-        position = 0
-        # Warn if implicitly selecting one among others. (Not sure if this scoring even exists, but here it goes)
-        if len(f[keys[0]]) > 1:
-            print("No position selected when multiple are available. Plotting the first one.")
-    # b'   0.00000000      < Energy <=    5.00000007E-02'
-    number_pattern = r'[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?'  # A pattern matching floats
-    for d in keys:
-        y.append(f[d][position])
-        tag = f[d].attrs["TAG"][0].decode('UTF-8')  # E.g., '   0.00000000      < Energy <=    5.00000007E-02'
-        values = list(map(float, re.findall(number_pattern, tag)))
-        x.append(sum(values) / len(values))
-    # TODO: First and last datasets are not bounded intervals. Their only finite point is used as the x coordinate, which must be understood with caution
-
-    plot_data, = ax.plot(x, y)
-    ax.set_xlim(min(x), max(x))
-
-    # Prepare a function for the updates
-    def update(i):
-        """Update the plot, returning the artists which must be redrawn"""
-        f = h5py.File(file_list[i], "r")
-        y = []
-        for d in keys:
-            y.append(f[d][position])
-        label = 't = {0}'.format(time_list[i])
-        plot_data.set_ydata(y)
-        ax.set_title(label)
-        if not scale_mode:
-            pass
-        elif scale_mode == "expand":
-            prev = ax.get_ylim()
-            data_limit = [min(y), max(y)]
-            ax.set_ylim(min(prev[0], data_limit[0]), max(prev[1], data_limit[1]))
-        elif scale_mode == "adjust_always":
-            ax.set_ylim(min(y), max(y))
-        return plot_data, ax
-
-    if not output_path:  # "" or None
-        # TODO: Plot in matplotlib window seems not to be working now. Perhaps jupyter-related.
-        FuncAnimation(fig, update, frames=np.arange(0, len(file_list)), interval=1)
-        plt.show()
-    elif output_path.split(".")[-1].lower() == "gif":
-        anim = FuncAnimation(fig, update, frames=np.arange(0, len(file_list)), interval=200)
-        anim.save(output_path, dpi=dpi, writer='imagemagick')
-    else:
-        metadata = dict(title=os.path.split(data_path)[-1], artist='duat', comment=data_path)
-        writer = FFMpegWriter(fps=fps, metadata=metadata)
-        with writer.saving(fig, output_path, dpi):
-            # Iterate over frames
-            for i in np.arange(0, len(file_list)):
-                update(i)
-                writer.grab_frame()
-            # Keep showing the last frame for the fixed time
-            writer.grab_frame()
-    plt.close()
 
 
 # TODO: Recode. DEPRECATED
@@ -572,10 +514,11 @@ def auto_process(run_dir=".", file_format="mp4", output_dir=None, verbose=None, 
                 v_print("- Generating: " + filename_base + "_" + str(c) + "." + file_format)
                 if num_threads:
                     t.add_call(
-                        Call(d.time_1d_animation, filename_base + "_" + str(c) + "." + file_format, dataset=c,
+                        Call(d.time_1d_animation, filename_base + "_" + str(c) + "." + file_format, dataset_selector=c,
                              **kwargs_1d))
                 else:
-                    d.time_1d_animation(filename_base + "_" + str(c) + "." + file_format, dataset=c, **kwargs_1d)
+                    d.time_1d_animation(filename_base + "_" + str(c) + "." + file_format, dataset_selector=c,
+                                        **kwargs_1d)
                 generated += 1
 
     if num_threads:
